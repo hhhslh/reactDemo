8140a7057a0ad79bf2c1e2375fb86c6cff68e2dd
hhhslh <3064063267@qq.com>
2019-09-24 17:44:14 +0800

    s0924

--- src/Menu.js
+++ src/Menu.js
@@ -1,6 +1,10 @@
 import React, {Component,Fragment} from 'react'
+import './menu.css'
+import Menuitem from './Menuitem'
 
 class Menu extends Component{
+    // 生命周期:在某一时刻可以自动执行的函数
+    // 构造函数 ES6的基本语法 可以看成是React的Initialization阶段，定义属性（props）和状态(state)
     constructor(props){
         super(props)
         this.state={
@@ -8,17 +12,69 @@ class Menu extends Component{
             list:['锅包肉','溜肉段']
         }
     }
+
+    componentWillMount(){
+        console.log('componentWillMount---组件将要挂载到页面的时刻')
+    }
+    componentDidMount(){
+        console.log('componentDidMount---组件挂载完成的时刻')
+    }
+    // 优化性能
+    shouldComponentUpdate(){
+        console.log('shouldComponentUpdate---组件更新之前')
+        return true
+        // false下面的内容不执行
+        // return false
+    }
+    componentWillUpdate(){
+        console.log('componentWillUpdate---shouldComponentUpdate之后执行')
+    }
+    componentDidUpdate(){
+        console.log('componentDidUpdate---组件更新之后')
+    }
+    // componentWillReceiveProps(){
+    //     console.log('componentWillReceiveProps')
+    // }
     render(){
+        console.log('render---组件挂载中')
         return(
+            // flex
             <Fragment>
+                {/*jsx注释*/}
                 <div>
-                    <input value={this.state.inputValue} onChange={this.inputChange.bind(this)}/>
+                    <label htmlFor="add">加菜：</label>
+                    <input 
+                        id="add" 
+                        className="inputBox" 
+                        value={this.state.inputValue} 
+                        onChange={this.inputChange.bind(this)}
+                        ref={input=>{this.input=input}}
+                    />
                     <button onClick={this.addList.bind(this)}>加菜</button>
                 </div>
-                <ul>
+                <ul ref={ul=>{this.ul=ul}}>
                     {
                         this.state.list.map((item,index)=>{
-                            return <li key={index+item}>{item}</li>
+                            return (
+                                <Menuitem 
+                                    key={index+item}
+                                    content={item}
+                                    index={index}
+                                    // list={this.state.list}
+                                    deleteItem={this.deleteItem.bind(this)}
+                                />
+
+                                // {/*
+                                //     <li 
+                                //         key={index+item}
+                                //         onClick={this.deleteItem.bind(this,index)}
+                                //         dangerouslySetInnerHTML={{__html:item}}
+                                //     >
+                                //     </li>
+
+                                // */}
+                                
+                            )
                         })
                     }
                 </ul>
@@ -26,18 +82,32 @@ class Menu extends Component{
         )
     }
 
-    inputChange(e){
+    inputChange(){
         this.setState({
-            inputValue:e.target.value
+            // inputValue:e.target.value
+            inputValue:this.input.value
         })
     }
     // 增加列表
     addList(){
+        // 异步
         this.setState({
             // 扩展...
             list:[...this.state.list,this.state.inputValue],
             inputValue:''
-            // list:['东北菜','川菜',this.state.inputValue]
+            // list:['锅包肉','溜肉段',this.state.inputValue]
+        },()=>{
+            console.log(this.ul.querySelectorAll("li").length)
+        })
+        
+    }
+    // 删除列表项
+    deleteItem(index){
+        // this.state.list.splice(index,1)性能优化障碍，React是禁止直接操作state的
+        let list=this.state.list
+        list.splice(index,1)
+        this.setState({
+            list:list
         })
     }
 
---
+++ src/Menuitem.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Menuitem extends Component {
+    constructor(props){
+        super(props)
+        this.handleClick=this.handleClick.bind(this)
+    }
+
+    //组件第一次存在于DOM中，函数是不会被执行的
+    //如果已经存在于DOM中，函数才会被执行
+    componentWillReceiveProps(){
+        console.log('child---componentWillReceiveProps')
+    }
+    componentWillUnmount(){
+        console.log('child---componentWillUnmount---组件删除时执行')
+    }
+
+    render() {
+        return (
+            <li onClick={this.handleClick}>
+            {this.props.waiterName}为你上菜-{this.props.content}</li>
+        );
+    }
+    handleClick(){
+        this.props.deleteItem(this.props.index)
+        // this.props.list=[]不能改变
+    }
+
+}
+
+// propTypes校验传递值
+Menuitem.propTypes={
+    // isRequired必须传递
+    waiterName:PropTypes.string.isRequired,
+    content:PropTypes.string,
+    index:PropTypes.number,
+    deleteItem:PropTypes.func
+}
+Menuitem.defaultProps={
+    waiterName:"孙晓二"
+}
+
+export default Menuitem;
+
+
